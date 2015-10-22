e579043fa0c0140aa1f2fbae3b90d99ad35985cf
Tom MacWright <tom@macwright.org>
2015-10-22 08:50:40 -0400

    First

---
+++ README.md
@@ -0,0 +1,3 @@
+# geojson-xyz-data
+
+Metadata that points at GeoJSON downloadable by geojson-xyz.
---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'naturalearth-3.3.0': {
+    files: require('./naturalearth-3.3.0/files.json'),
+    sizes: require('./naturalearth-3.3.0/sizes.json')
+  }
+};
