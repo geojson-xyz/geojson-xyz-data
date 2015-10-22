665edf0da42ed272b115a8375a61319b1f3106b3
Tom MacWright <tom@macwright.org>
2015-10-22 11:33:01 -0400

    Add summaries

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   'naturalearth-3.3.0': {
     files: require('./naturalearth-3.3.0/files.json'),
     sizes: require('./naturalearth-3.3.0/sizes.json'),
+    summaries: require('./naturalearth-3.3.0/summaries.json'),
     meta: require('./naturalearth-3.3.0/meta.json')
   }
 };
