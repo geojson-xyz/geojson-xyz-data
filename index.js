57b6e56f80c206ab3e5f35da04bb4bdc9064bb91
Anand Thakker <vestibule@anandthakker.net>
2016-01-23 13:04:58 -0500

    Clean up index-building and add to postinstall

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+var path = require('path');
 module.exports = {
   'naturalearth-3.3.0': {
     files: require('./naturalearth-3.3.0/files.json'),
-    meta: require('./naturalearth-3.3.0/meta.json')
+    meta: require('./naturalearth-3.3.0/meta.json'),
+    index: path.join(__dirname, 'naturalearth-3.3.0', 'name-index.csv')
   }
 };
